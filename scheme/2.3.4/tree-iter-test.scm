9bd82926e52bd75a466bb1bfb7778eafa2d05f82
Anton Baukin <anton.baukin@gmail.com>
2019-02-17 20:12:19 +0300

    SICP-2 Implemented tree max-min and min-max functions.

--- scheme/2.3.4/tree-iter-test.scm
+++ scheme/2.3.4/tree-iter-test.scm
@@ -7,16 +7,19 @@
 (define NumTree (make-tree <))
 (define num-tree->str (make-tree->str-printer NumTree number->string))
 (define num-list->tree (tree-op<-list NumTree))
+(define num-tree->list (tree-op->list NumTree))
 (define num-tree-order-iter (make-tree-order-iter NumTree))
-(define num-tree-tree-get-min (make-tree-get-min NumTree))
-(define num-tree-tree-get-max (make-tree-get-max NumTree))
+(define num-tree-get-min (make-tree-get-min NumTree))
+(define num-tree-get-max (make-tree-get-max NumTree))
+(define num-tree-get-max-smaller (make-tree-get-max-smaller NumTree))
+(define num-tree-get-min-greater (make-tree-get-min-greater NumTree))
 
 (define (num-tree-collector tree)
  (define result '())
 
  (num-tree-order-iter tree (lambda (v)
   (set! result (cons v result))
-  '() ;<— always continue
+  void ;<— always continue
  ))
 
  (reverse result)
@@ -30,6 +33,24 @@
  (iter n (list))
 )
 
+(define (filter sequence match?)
+ (define (next tail res)
+  (if (null? tail) res
+   (next (cdr tail)
+    (if (match? (car tail)) (cons (car tail) res) res)
+   )
+  )
+ )
+
+ (reverse (next sequence '()))
+)
+
+(define (drop-equal-nums sequence mod)
+ (if (<= mod 1) sequence
+  (filter sequence (lambda (x) (not (= 0 (remainder x mod)))))
+ )
+)
+
 (define (test-tree-collect n)
  (let ((tree (num-list->tree (enumerate n))))
   (log "\nTree " (num-tree-collector tree) "\n" (num-tree->str tree) "\n")
@@ -37,13 +58,68 @@
 )
 
 (define (test-tree-min tree)
- (log "\nTree min = " (num-tree-tree-get-min tree) "\n" (num-tree->str tree) "\n")
+ (log "\nTree min = " (num-tree-get-min tree) "\n" (num-tree->str tree) "\n")
 )
 
 (define (test-tree-max tree)
- (log "\nTree max = " (num-tree-tree-get-max tree) "\n" (num-tree->str tree) "\n")
+ (log "\nTree max = " (num-tree-get-max tree) "\n" (num-tree->str tree) "\n")
 )
 
-;(for-each test-tree-collect (enumerate 10))
+(define (tree-get-max-smaller n)
+ (define (get-smaller tree i)
+  (let ((sm (num-tree-get-max-smaller tree i)))
+   (if (null? sm) "NO" (number->string sm))
+  )
+ )
+
+ (define (num-test tree i res)
+  (if (> i (+ n 1)) res
+   (num-test tree (+ i 1) (string-append res " "
+    (number->string i) "→" (get-smaller tree i)
+   ))
+  )
+ )
+ ;(drop-equal-nums sequence mod)
+ (let* (
+   (items (drop-equal-nums (enumerate n) (quotient n 3)))
+   (tree (num-list->tree items))
+  )
+  (log "\nMax-smaller of tree " (num-tree->list tree)
+   (num-test tree 1 "") "\n"
+   (num-tree->str tree) "\n"
+  )
+ )
+)
+
+(define (tree-get-min-greater n)
+ (define (get-greater tree i)
+  (let ((gr (num-tree-get-min-greater tree i)))
+   (if (null? gr) "NO" (number->string gr))
+  )
+ )
+
+ (define (num-test tree i res)
+  (if (> i n) res
+   (num-test tree (+ i 1) (string-append res " "
+    (number->string i) "→" (get-greater tree i)
+   ))
+  )
+ )
+ ;(drop-equal-nums sequence mod)
+ (let* (
+   (items (drop-equal-nums (enumerate n) (quotient n 3)))
+   (tree (num-list->tree items))
+  )
+  (log "\nMin-greater of tree " (num-tree->list tree)
+   (num-test tree 0 "") "\n"
+   (num-tree->str tree) "\n"
+  )
+ )
+)
+
+
+(for-each test-tree-collect (enumerate 10))
 (test-tree-min (num-list->tree (enumerate 10)))
 (test-tree-max (num-list->tree (enumerate 10)))
+(for-each tree-get-max-smaller (enumerate 10))
+(for-each tree-get-min-greater (enumerate 10))
--- scheme/2.3.4/tree-iter.scm
+++ scheme/2.3.4/tree-iter.scm
@@ -1,3 +1,4 @@
+(include "../2.3.3/quick-sort.scm")
 
 ; Creates iteration function for the given tree.
 ; The function takes (tree iter), where «iter» is
@@ -8,37 +9,42 @@
 ; up-nodes of the current path. «From» is previously
 ; invoked node: initially is '(), on going down it's
 ; the top of the stack, on returning up — the left
-; or the right node. If going to the requested direction
-; is not possible, iteration is breaked returning pair
-; (last node . stack)
+; or the right node. If going to the requested way
+; is not possible, iteration is breaked returning
+; list ('break way node from stack). On else result,
+; list ('result result node from stack) is returned.
 (define (make-tree-iter treeops)
  (define left (tree-op-get-left treeops))
  (define right (tree-op-get-right treeops))
 
+ (define (break way node from stack)
+  (list 'break way node from stack)
+ )
+
  (lambda (tree iter)
   (define (navigate node from stack)
-   (let ((dir (iter node from stack)))
+   (let ((way (iter node from stack)))
     (cond
 
-     ((eq? 'left dir)
-      (if (null? (left node)) node
+     ((eq? 'left way)
+      (if (null? (left node)) (break way node from stack)
        (navigate (left node) node (cons node stack))
       )
      )
 
-     ((eq? 'right dir)
-      (if (null? (right node)) node
+     ((eq? 'right way)
+      (if (null? (right node)) (break way node from stack)
        (navigate (right node) node (cons node stack))
       )
      )
 
-     ((eq? 'up dir)
-      (if (null? stack) node
+     ((eq? 'up way)
+      (if (null? stack) (break way node from stack)
        (navigate (car stack) node (cdr stack))
       )
      )
 
-     (else (cons node stack))
+     (else (list 'result way node from stack))
     )
    )
   )
@@ -50,7 +56,7 @@
 ; Creates iteration function that walks the tree from the
 ; minimum item to the maximum. The function takes (tree iter),
 ; where «iter» is function taking the value (item) of a node.
-; When iterator returns '() it asks to continue, else value
+; When iterator returns void it asks to continue, else value
 ; is treated as overall iteration result, it breaks iteration.
 (define (make-tree-order-iter treeops)
  (define get (tree-op-get treeops))
@@ -76,8 +82,8 @@
 
  (define (call-or-go iter node dir)
   (let ((res (iter (get node))))
-   ;(log "go " (if (null? res) dir res))
-   (if (null? res) dir res)
+   ;(log "go " (if (eq? void res) dir res))
+   (if (eq? void res) dir res)
   )
  )
 
@@ -128,39 +134,149 @@
 ; For the given tree find the minimum (left-most) item.
 (define (make-tree-get-min treeops)
  (define get (tree-op-get treeops))
- (define left (tree-op-get-left treeops))
  (define tree-iter (make-tree-iter treeops))
 
- (define (left? node)
-  (not (null? (left node)))
+ (define (result r)
+  (get (caddr r))
  )
 
  (lambda (tree)
-  (get (car (tree-iter tree (lambda (node from stack)
-   (if (left? node) 'left node)
-  ))))
+  (result (tree-iter tree (lambda (node from stack) 'left)))
  )
 )
 
 ; For the given tree find the maximum (right-most) item.
 (define (make-tree-get-max treeops)
  (define get (tree-op-get treeops))
- (define right (tree-op-get-right treeops))
  (define tree-iter (make-tree-iter treeops))
 
- (define (right? node)
-  (not (null? (right node)))
+ (define (result r)
+  (get (caddr r))
  )
 
  (lambda (tree)
-  (get (car (tree-iter tree (lambda (node from stack)
-   (if (right? node) 'right node)
-  ))))
+  (result (tree-iter tree (lambda (node from stack) 'right)))
  )
 )
 
-; For the given (tree item) find the largest
+; For the given (tree item) finds the largest
 ; item that is smaller than the given one, i.e.,
-; the previous. The given item may absent.
-(define (make-tree-find-smaller treeops)
+; the previous one. The given item may absent.
+(define (make-tree-get-max-smaller treeops)
+ (define get (tree-op-get treeops))
+ (define left (tree-op-get-left treeops))
+ (define smaller? (tree-op-smaller? treeops))
+ (define tree-iter (make-tree-iter treeops))
+ (define get-max (make-tree-get-max treeops))
+
+ (define (smaller-node? a b)
+  (smaller? (get a) (get b))
+ )
+
+ (define (get-max-safe tree)
+  ;(log "max " (if (null? tree) '() (get-max tree)) " of " tree)
+  (if (null? tree) '() (get-max tree))
+ )
+
+ (define (min-max item node stack)
+  (define (next res tail)
+   (cond
+    ((null? tail) res)
+
+    ((smaller? (get (car tail)) item)
+     (next (get (car tail)) (cdr tail))
+    )
+
+    (else res)
+   )
+  )
+
+  ;(log "min-max " item " node = " node " stack = " stack)
+  (next '() (quick-sort smaller-node? (cons node stack)))
+ )
+
+ ; When going left we put to the stack an item that is greater,
+ ; right — the item is smaller. So, when breaked, we have stack
+ ; that contains smaller and greater items, and we do min-max.
+ (define (result item r)
+  ;(log "result " item " " r)
+
+  (if (and (eq? 'result (car r)) (not (null? (cadr r))))
+   (cadr r) (min-max item (list-ref r 2) (list-ref r 4))
+  )
+ )
+
+ (lambda (tree item)
+  (result item (tree-iter tree (lambda (node from stack)
+   ;(log "item = " item " node = " node)
+
+   (cond
+    ((smaller? item (get node)) 'left)
+    ((smaller? (get node) item) 'right)
+
+    ; { node's item equals to the item}
+    (else (get-max-safe (left node)))
+   )
+  )))
+ )
+)
+
+
+; For the given (tree item) finds the smallest
+; item that is greater than the given one, i.e.,
+; the next one. The given item may absent.
+(define (make-tree-get-min-greater treeops)
+ (define get (tree-op-get treeops))
+ (define right (tree-op-get-right treeops))
+ (define smaller? (tree-op-smaller? treeops))
+ (define tree-iter (make-tree-iter treeops))
+ (define get-min (make-tree-get-min treeops))
+
+ (define (greater-node? a b)
+  (smaller? (get b) (get a))
+ )
+
+ (define (get-min-safe tree)
+  ;(log "min " (if (null? tree) '() (get-min tree)) " of " tree)
+  (if (null? tree) '() (get-min tree))
+ )
+
+ (define (max-min item node stack)
+  (define (next res tail)
+   (cond
+    ((null? tail) res)
+
+    ((smaller? item (get (car tail)))
+     (next (get (car tail)) (cdr tail))
+    )
+
+    (else res)
+   )
+  )
+
+  ;(log "nin-max " item " node = " node " stack = " stack)
+  (next '() (quick-sort greater-node? (cons node stack)))
+ )
+
+ (define (result item r)
+  ;(log "result " item " " r)
+
+  (if (and (eq? 'result (car r)) (not (null? (cadr r))))
+   (cadr r) (max-min item (list-ref r 2) (list-ref r 4))
+  )
+ )
+
+ (lambda (tree item)
+  (result item (tree-iter tree (lambda (node from stack)
+   ;(log "item = " item " node = " node)
+
+   (cond
+    ((smaller? item (get node)) 'left)
+    ((smaller? (get node) item) 'right)
+
+    ; { node's item equals to the item}
+    (else (get-min-safe (right node)))
+   )
+  )))
+ )
 )
