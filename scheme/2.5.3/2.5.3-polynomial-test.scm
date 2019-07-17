a9206f7cb1d476cf6dec456f5d30d2815d51ddae
Anton Baukin <anton.baukin@gmail.com>
2019-07-17 15:56:09 +0300

    SICP-2 Finished polynomial to string formatter for §2.5.3.

--- scheme/2.5.3/2.5.3-polynomial-test.scm
+++ scheme/2.5.3/2.5.3-polynomial-test.scm
@@ -1,4 +1,4 @@
-(include "../2.5.2/2.5.2-arithmetics.scm")
+(include "2.5.3-arithmetics.scm")
 (include "2.5.3-polynomial.scm")
 
 (define (log . args) (for-each display args) (newline))
@@ -8,7 +8,8 @@
  install-polynomial-package
 )
 
-(define N make-number) ;<— shortcuts...
+(define N make-number)  ;<— shortcuts...
+(define I make-integer)
 (define P (cadr polynomial-package))
 
 (define (log-poly p)
@@ -16,4 +17,15 @@
 )
 
 (log "2x + 3 = " (P 'x 1 2 0 3))
-(log-poly (P 'x 2 (N 4) 1 (N 2) 0 (N 3)))
+
+; Prints 4x² + 2x + 3 using superscript for powers:
+(log-poly (P 'x 2 (N 4) 1 (N 2) 0 3))
+
+; Prints x² - x + 1 omitting one-coefficients.
+(log-poly (P 'x 2 (N 1) 1 (I -1) 0 1))
+
+; Prints -4x² - 2x + 3 extracting «-» top omit «+».
+(log-poly (P 'x 2 (N -4) 1 -2 0 3))
+
+; Prints 10x² + (y² + 2)x + 3 with polynimial coeffs.
+(log-poly (P 'x 2 10 1 (P 'y 2 1 0 2) 0 3))
--- scheme/2.5.3/2.5.3-polynomial.scm
+++ scheme/2.5.3/2.5.3-polynomial.scm
@@ -107,16 +107,67 @@
   )
  )
 
- (define (term->str v o c s)
-  (let* (
-    (cs (coeff->str c))
-    ;–> coeff sign integer, leading term has no sign.
-    ;(sg (if (= 0 (length s)) 0 (coeff->sign c)))
+ (define (digit? c)
+  (let ((s (string c)))
+   (and
+    (string>=? s "0")
+    (string<=? s "9")
    )
+  )
+ )
 
+ (define (needs-braces-iter s i l)
+  (if (= i l) #f
+   (let ((c (string-ref s i)))
+    (if (or (eq? c #\.) (eq? c #\-) (digit? c))
+     (needs-braces-iter s (+ i 1) l)
+     #t
+    )
+   )
+  )
+ )
+
+ (define (needs-braces? cs)
+  (needs-braces-iter cs 0 (string-length cs))
+ )
+
+ ; Simplified version of plain one number coefficient test.
+ (define (omit-one o cs)
+  (if (= 0 o) cs ;<— not omit one coeff for the last term (0-power)
+   (if (or (equal? cs "1") (equal? cs "1.") (equal? cs "1.0")) "" cs)
+  )
+ )
+
+ ; Plus-junction for the terms. For coefficients starting
+ ; with «-» prints minus-junction omitting the coeff sign.
+ (define (plus-coeff o cs)
+  (let ((x (string-ref cs 0)))
+   (if (eq? x #\-)
+    (string-append " - " (omit-one o (substring cs 1 (string-length cs))))
+    (string-append " + " (omit-one o cs))
+   )
+  )
+ )
+
+ (define (term->str v o c s)
+  (let ((cs (coeff->str c)))
    (string-append
-    (if (= 0 (string-length s )) "" " + ")
-    cs
+    (cond
+     ; Coeff is not a plain number format?
+     ((needs-braces? cs)
+      (string-append
+       (if (= 0 (string-length s)) "" " + ")
+       "(" cs ")"
+      )
+     )
+
+     ; It's a first coeff?
+     ((= 0 (string-length s)) (omit-one o cs))
+
+     ; Add it via a junction.
+     (else (plus-coeff o cs))
+    )
+
     (order->str v o)
    )
   )
